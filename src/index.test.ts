51382e42e6bbdadb37e783d40396113a7e60b663
Pat-Up <patrick@upforjobs.com>
2021-04-21 14:21:03 +0700

    feat(index): the meat of the package

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,32 @@
 import test from 'ava'
+import { negative } from './index'
+interface Test {
+  name: string
+  input: number,
+  expected: number
+}
 
-test('basic test', (t) => {
-  t.pass()
+const data: Test[] = [
+  {
+    name: 'positive number',
+    input: 1,
+    expected: -1
+  },
+  {
+    name: 'negative number',
+    input: -1,
+    expected: -1
+  },
+  {
+    name: 'handles zero',
+    input: 0,
+    expected: 0
+  }
+]
+
+data.forEach(({ name, input, expected }) => {
+  test(name, (t) => {
+    const actual = negative(input)
+    t.is(actual, expected)
+  })
 })
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export const negative = (num: number): number => num <= 0 ? num : -(num)
